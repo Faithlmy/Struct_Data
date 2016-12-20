3eb18d1b52edf6e497ebdf817373d11c0a248b00
lmy <lmengyy@126.com>
2016-12-21 00:35:02 +0800

    d

--- Pt1/Pt1/0.cpp
+++ Pt1/Pt1/0.cpp
@@ -65,7 +65,7 @@ void exchange_rootLast(Tree* T)  // 后序遍历
 
 
 }
-Tree * exchange_rootMiddle(Tree* T)     //类似于 中序遍历
+Tree * exchange_rootMiddle(Tree* T)   //类似于 中序遍历
 {
 	if (T != NULL)
 	if (T->LChild != NULL || T->RChild != NULL)
